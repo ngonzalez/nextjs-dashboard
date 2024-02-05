2d248f783b206a828bae6de2c58f7cf5749e94e8
Nicolas GONZALEZ <ngonzalez@warmango.fr>
2024-02-05 12:51:10 +0100

    Set cors headers

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,16 @@
 import { NextResponse } from "next/server";
 
 export function middleware() {
-    const res = NextResponse.next()
+  const response = NextResponse.next()
 
-    res.headers.append('Access-Control-Allow-Credentials', "true")
-    res.headers.append('Access-Control-Allow-Origin', '*')
-    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
-    res.headers.append('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version')
+  response.headers.append('Access-Control-Allow-Credentials', "true")
+  response.headers.append('Access-Control-Allow-Origin', '*')
+  response.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+  response.headers.append('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version')
 
-    return res
+  return response
 }
 
 export const config = {
-    matcher: ['/:path*'],
+  matcher: ['/:path*'],
 }
